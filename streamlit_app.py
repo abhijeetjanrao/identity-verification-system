f4a1aa39aed00ddfd6182ecdb1823e6f1d27f4c1
abhijeetjanrao <abhijeetjanrao2@gmail.com>
2026-08-26 21:13:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ with col1:
 
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption="Uploaded image", use_column_width=True)
+        st.image(image, caption="Uploaded image", use_container_width=True)
 
 with col2:
     st.subheader("How it works")
